f5619d1f296d9997a6a60618df40158fdbb5b264
Matt Dailey <matthew.j.dailey@gmail.com>
2017-12-30 12:55:59 -0800

    run something as a job service

---
+++ app/src/main/java/com/reactiverobot/nudge/CheckActiveAppJobService.java
@@ -0,0 +1,21 @@
+package com.reactiverobot.nudge;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.util.Log;
+
+public class CheckActiveAppJobService extends JobService {
+
+    public static final String TAG = CheckActiveAppJobService.class.getName();
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "hi from job");
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
--- app/src/main/java/com/reactiverobot/nudge/MainActivity.java
+++ app/src/main/java/com/reactiverobot/nudge/MainActivity.java
@@ -1,13 +1,18 @@
 package com.reactiverobot.nudge;
 
 import android.app.ActivityManager;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
 import android.app.usage.UsageEvents;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -41,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
             Log.d(TAG, "isInactive : " + appInactive);
         }
+
+        JobScheduler jobService = (JobScheduler) this.getSystemService(JOB_SCHEDULER_SERVICE);
+
+        JobInfo jobInfo = new JobInfo.Builder(1001, new ComponentName(this, CheckActiveAppJobService.class))
+                .setMinimumLatency(5000)
+                .build();
+
+        jobService.schedule(jobInfo);
 
         Log.d(TAG, "Usage stats" + usageStats);
     }
