757d087eab56368ec140c32fb9cc1121006ef241
Matt Dailey <matthew.j.dailey@gmail.com>
2017-12-30 16:18:36 -0800

    allow opening important apps from kindle

--- app/src/main/java/com/reactiverobot/nudge/SuggestChangeActivity.java
+++ app/src/main/java/com/reactiverobot/nudge/SuggestChangeActivity.java
@@ -1,14 +1,38 @@
 package com.reactiverobot.nudge;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SuggestChangeActivity extends Activity {
+
+    private void launchApplicationAndClose(String packageName) {
+        Intent i = getPackageManager().getLaunchIntentForPackage(packageName);
+        startActivity(i);
+
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggest_change);
+
+        findViewById(R.id.button_open_option_1)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        launchApplicationAndClose("com.amazon.kindle");
+                    }
+                });
+
+        findViewById(R.id.button_open_option_2)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        launchApplicationAndClose("io.spire.android");
+                    }
+                });
     }
 }
