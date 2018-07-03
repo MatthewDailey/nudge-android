16eea293d719f5db67b5b921a61fbae22a1db8a8
Matt Dailey <mjd@figma.com>
2018-07-02 23:39:43 -0400

    add animation and fab

--- app/src/main/java/com/reactiverobot/nudge/MainActivity.java
+++ app/src/main/java/com/reactiverobot/nudge/MainActivity.java
@@ -5,8 +5,15 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TabHost;
 import android.widget.TextView;
@@ -116,5 +123,51 @@ public class MainActivity extends AppCompatActivity {
         TextView titleView = findViewById(R.id.title_text_view);
         Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/Pacifico-Regular.ttf");
         titleView.setTypeface(typeFace);
+
+        host.setOnTabChangedListener(s -> {
+            if (s == "Tab One") {
+                animateFab(0);
+            } else {
+                animateFab(1);
+            }
+        });
+    }
+
+    int[] colorIntArray = {R.color.colorPrimary, R.color.colorPrimary};
+    int[] iconIntArray = {android.R.drawable.ic_input_add, android.R.drawable.ic_input_add};
+
+    protected void animateFab(final int position) {
+        FloatingActionButton fab = findViewById(R.id.fab_select_package);
+
+        fab.clearAnimation();
+        // Scale down animation
+        ScaleAnimation shrink =  new ScaleAnimation(1f, 0.2f, 1f, 0.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        shrink.setDuration(150);     // animation duration in milliseconds
+        shrink.setInterpolator(new DecelerateInterpolator());
+        shrink.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Change FAB color and icon
+                fab.setBackgroundTintList(getResources().getColorStateList(colorIntArray[position]));
+                fab.setImageDrawable(getResources().getDrawable(iconIntArray[position], null));
+
+                // Scale up animation
+                ScaleAnimation expand =  new ScaleAnimation(0.2f, 1f, 0.2f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                expand.setDuration(100);     // animation duration in milliseconds
+                expand.setInterpolator(new AccelerateInterpolator());
+                fab.startAnimation(expand);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        fab.startAnimation(shrink);
     }
 }
