1ec494b82b8332faf50e519c79d489dde20f3c01
Matt Dailey <mjd@figma.com>
2018-08-10 15:10:17 -0700

    add clicking to to test breath activity

--- app/src/main/java/com/reactiverobot/nudge/info/BreathingActivity.java
+++ app/src/main/java/com/reactiverobot/nudge/info/BreathingActivity.java
@@ -5,12 +5,13 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.TransitionManager;
+import android.view.View;
 
 import com.reactiverobot.nudge.R;
 
-import dagger.android.AndroidInjection;
-
 public class BreathingActivity extends AppCompatActivity {
+
+    int currentLayout = R.layout.activity_breathing_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,15 @@ public class BreathingActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_breathing_1);
 
-        changeToScene(R.layout.activity_breathing_2);
+        final ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout_breathing);
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentLayout = currentLayout == R.layout.activity_breathing_1 ?
+                        R.layout.activity_breathing_2 : R.layout.activity_breathing_1;
+                changeToScene(currentLayout);
+            }
+        });
     }
 
     private void changeToScene(int activity_resource) {
