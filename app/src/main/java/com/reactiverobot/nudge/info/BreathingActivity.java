a1b455926f90eb3a98bb839de389bbbc9eee11bc
Matt Dailey <mjd@figma.com>
2018-08-12 16:03:51 -0400

    breathing animation

--- app/src/main/java/com/reactiverobot/nudge/info/BreathingActivity.java
+++ app/src/main/java/com/reactiverobot/nudge/info/BreathingActivity.java
@@ -1,42 +1,64 @@
 package com.reactiverobot.nudge.info;
 
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.TransitionManager;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
 import com.reactiverobot.nudge.R;
 
 public class BreathingActivity extends AppCompatActivity {
 
-    int currentLayout = R.layout.activity_breathing_1;
+    public static final int SCALE_FACTOR = 10;
+    int currentLayout = R.layout.activity_breathing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_breathing_1);
+        setContentView(R.layout.activity_breathing);
 
-        final ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout_breathing);
-        constraintLayout.setOnClickListener(new View.OnClickListener() {
+        View block = findViewById(R.id.block);
+
+        ScaleAnimation scaleOut = new ScaleAnimation(1, SCALE_FACTOR, 1, SCALE_FACTOR, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleOut.setDuration(4000);
+
+        ScaleAnimation scaleIn = new ScaleAnimation(SCALE_FACTOR, 1, SCALE_FACTOR, 1,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleIn.setDuration(4000);
+
+        scaleOut.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onClick(View view) {
-                currentLayout = currentLayout == R.layout.activity_breathing_1 ?
-                        R.layout.activity_breathing_2 : R.layout.activity_breathing_1;
-                changeToScene(currentLayout);
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                block.startAnimation(scaleIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
+
+        scaleIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                block.startAnimation(scaleOut);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
             }
         });
-    }
 
-    private void changeToScene(int activity_resource) {
-        final ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout_breathing);
-
-        final ConstraintSet constraintSet3 = new ConstraintSet();
-        constraintSet3.clone(getApplicationContext(), activity_resource);
-
-        TransitionManager.beginDelayedTransition(constraintLayout);
-        constraintSet3.applyTo(constraintLayout);
+        block.startAnimation(scaleOut);
     }
 }
